d2900b2fb507fdcb34df1590f7a1fa5254047651
Mohamed Agnide <fadulagnide01@gmail.com>
2024-12-09 11:43:21 +0100

    Resource catégories effacées

--- routes/web.php
+++ routes/web.php
@@ -38,7 +38,7 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-// Route::resource('categories', CategoryController::class);
+
 
 
 require __DIR__.'/auth.php';
